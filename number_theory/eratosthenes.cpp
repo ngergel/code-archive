47fe25150a88dfc0cae9db988ba43acd786845d4
Noah Gergel <n.gergel1@gmail.com>
2020-04-07 12:15:33 -0600

    Formatting oop

--- number_theory/eratosthenes.cpp
+++ number_theory/eratosthenes.cpp
@@ -1,16 +1,16 @@
 /*
-	Sieve of Eratosthenes
-	==================================
+    Sieve of Eratosthenes
+    ==================================
 
-	Description:
-		Finds all primes in range [2, n], for some n.
-		Also grabs a prime divisor for composite numbers.
-	
-	Complexity:
-		- Time: O(n log log n)
-		- Space: O(n)
+    Description:
+        Finds all primes in range [2, n], for some n.
+        Also grabs a prime divisor for composite numbers.
+    
+    Complexity:
+        - Time: O(n log log n)
+        - Space: O(n)
 
-	(c) Noah Gergel 2020
+    (c) Noah Gergel 2020
 */
 
 
@@ -28,43 +28,43 @@ using ll = long long;
 
 
 /*
-	Implementation of the sieve of Eratosthenes.
+    Implementation of the sieve of Eratosthenes.
 
-	Arguments:
-		- primes: Array that will be turned into list of primes/divisors.
-		- n: Upper bound of sieve.
+    Arguments:
+        - primes: Array that will be turned into list of primes/divisors.
+        - n: Upper bound of sieve.
 */
 void eratosthenes(vector<ll>& primes, ll n) {
-	primes.resize(n + 1); iota(begin(primes), end(primes), 0);
+    primes.resize(n + 1); iota(begin(primes), end(primes), 0);
 
-	for (ll i = 2; i * i <= n; i++) {
-		if (primes[i] == i) {
-			for (ll j = i + i; j <= n; j += i) {
-				primes[j] = i;
-			}
-		}
-	}
+    for (ll i = 2; i * i <= n; i++) {
+        if (primes[i] == i) {
+            for (ll j = i + i; j <= n; j += i) {
+                primes[j] = i;
+            }
+        }
+    }
 }
 
 
 // Driver code.
 int main() {
-	vector<ll> pr;
-	ll n;
+    vector<ll> pr;
+    ll n;
 
-	// Get the upper bound n.
-	cout << "An upper bound n: ";
-	cin >> n;
+    // Get the upper bound n.
+    cout << "An upper bound n: ";
+    cin >> n;
 
-	// Run the sieve.
-	eratosthenes(pr, n);
+    // Run the sieve.
+    eratosthenes(pr, n);
 
-	// Print results. Nicely formatted up to n ~ 40.
-	cout << " n:";
-	for (ll i = 0; i <= n; i++) cout << setw(3) << i;
-	cout << endl << "pr:";
-	for (ll i = 0; i <= n; i++) cout << setw(3) << pr[i];
-	cout << endl;
+    // Print results. Nicely formatted up to n ~ 40.
+    cout << " n:";
+    for (ll i = 0; i <= n; i++) cout << setw(3) << i;
+    cout << endl << "pr:";
+    for (ll i = 0; i <= n; i++) cout << setw(3) << pr[i];
+    cout << endl;
 
-	return 0;
+    return 0;
 }
